bcd7c2ad1648cb767338a05f5df2b674f1db9aa0
Christoph Ellis <christoph_ellis@outlook.com>
2018-10-20 19:08:06 +0000

    Finished ex 2

--- maze.js
+++ maze.js
@@ -1,8 +1,22 @@
 window.onload = function () {
+   /* Ex 1 
     document.getElementById("boundary1").addEventListener("mouseover", mouseOver);
     
     function mouseOver() {
          let ele = document.getElementById("boundary1");
          ele.classList.add("youlose");
         }
+    */
+    
+    
+    let bound = document.querySelectorAll("div.boundary");
+    
+    for (let i = 0; i < bound.length; i++){
+        
+        bound[i].addEventListener("mouseover", 
+            function() {
+                bound[i].classList.add("youlose");
+            })
+    };
+    
 }

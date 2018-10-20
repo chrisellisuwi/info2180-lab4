5eab99d550b0cfbf787100803e793aca587f1b8c
Christoph Ellis <christoph_ellis@outlook.com>
2018-10-20 20:46:47 +0000

    Completed lab

--- maze.js
+++ maze.js
@@ -8,15 +8,37 @@ window.onload = function () {
         }
     */
     
-    
+    //Ex 2
     let bound = document.querySelectorAll("div.boundary");
     
-    for (let i = 0; i < bound.length; i++){
-        
-        bound[i].addEventListener("mouseover", 
-            function() {
-                bound[i].classList.add("youlose");
-            })
-    };
+    function loseFunc() {
+        for (let j = 0; j < bound.length; j++)
+            bound[j].classList.add("youlose");
+        document.getElementById("status").innerHTML = "You Lose!"; // Ex 5
+    }
     
+    for (let i = 0; i < bound.length; i++)
+        bound[i].addEventListener("mouseover", loseFunc);
+    
+    //Ex 3
+    document.getElementById("end").addEventListener("mouseover", 
+        function (){
+            if (document.querySelectorAll(".youlose").length === 0){
+               // alert("You Win!");
+               document.getElementById("status").innerHTML = "You Win!"; //Ex 5
+            }
+        });
+        
+     //Ex 4 
+    document.getElementById("start").addEventListener("click", 
+        function (){
+            if (document.querySelectorAll(".youlose").length > 0){
+                let loss= document.querySelectorAll(".youlose");
+                for (let i = 0; i < loss.length; i++){
+                    loss[i].classList.remove("youlose");
+                }
+            }
+            document.getElementById("maze").addEventListener("mouseleave", loseFunc); //Ex 6
+            document.getElementById("status").innerHTML = 'Move your mouse over the "S" to begin.' //Ex 5
+        });
 }
